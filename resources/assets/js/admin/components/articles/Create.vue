98cf2294db6e1c99c5961fec63b267a0e077784d
lianginet <lianginet@gmail.com>
2016-12-06 17:10:24 +0800

    Add the full-screen exit event to create articles page

--- resources/assets/js/admin/components/articles/Create.vue
+++ resources/assets/js/admin/components/articles/Create.vue
@@ -1,8 +1,11 @@
 <template>
-        <div id="editor" class="full-screen">
-            <textarea v-model="input" debounce="2000" title=""></textarea>
-            <div v-html="compiledMarkdown"></div>
-        </div>
+    <div id="editor" :class="{ 'full-screen': isFullScreen }">
+        <textarea
+            v-model="input"
+            title="">
+        </textarea>
+        <div v-html="compiledMarkdown"></div>
+    </div>
 </template>
 
 <script type=text/ecmascript-6>
@@ -10,13 +13,36 @@
     export default {
         data() {
             return {
-                input: '#### title',
+                input: '### title',
+                isFullScreen: true
             }
         },
         computed: {
             compiledMarkdown() {
-                return marked(this.input, { sanitize: true })
+                return marked(this.input, {sanitize: true})
             }
+        },
+        methods: {
+            toggleFullScreen() {
+                this.isFullScreen = !this.isFullScreen
+                console.log('isFullScreen:' + this.isFullScreen)
+            },
+            escEventListener(event) {
+                if (27 === event.keyCode) {
+                    console.log('You clicked `esc` key')
+                    if (true === this.isFullScreen) {
+                        this.toggleFullScreen()
+                    }
+                }
+            }
+        },
+        created() {
+            // Add escEventListener
+            window.addEventListener('keyup', this.escEventListener)
+        },
+        destroyed() {
+            // Remove escEventListener
+            window.removeEventListener('keyup', this.escEventListener)
         }
     }
 </script>
@@ -31,10 +57,13 @@
         right: 0;
         background: #fff;
     }
+    #editor {
+        height: 100%;
+    }
     textarea, #editor div {
         display: inline-block;
         width: 49%;
-        height: 500px;
+        height: 100%;
         vertical-align: top;
         -webkit-box-sizing: border-box;
         -moz-box-sizing: border-box;
@@ -47,12 +76,10 @@
         border-right: 1px solid #ccc;
         resize: none;
         outline: none;
-        background-color: #f6f6f6;
         font-size: 14px;
         font-family: 'Monaco', courier, monospace;
         padding: 20px;
     }
-
     code {
         color: #f66;
     }
